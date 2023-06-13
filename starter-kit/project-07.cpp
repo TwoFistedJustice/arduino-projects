423368b80fe49b9913f868d10671b03a5360696a
TwoFistedJustice <22874887+TwoFistedJustice@users.noreply.github.com>
2023-06-12 21:42:25 -0400

    refactor: deprecate notes array in favor of note-named variables

--- starter-kit/project-07.cpp
+++ starter-kit/project-07.cpp
@@ -3,10 +3,15 @@
  * piezo on pin 8
  * */
 
-// set up array w 6 buttons
-//int buttons[6];
-//int buttons[0] = 2; // book has int type, IDE complains abou redeclaration
-int notes[] = {262, 294, 330, 349};
+//int notes[] = {262, 294, 330, 349};
+// refactor to note names
+int noteC = 262;
+int noteD = 294;
+int noteE = 330;
+int noteF = 349;
+
+
+
 void setup(){
     Serial.begin(9600);
 }
@@ -15,19 +20,19 @@ void loop(){
     Serial.println(keyVal);
     if(keyVal == 1023){
         // play C
-        tone(8,notes[0]);
+        tone(8,noteC);
     }
     else if(keyVal >= 990 && keyVal <= 1010){
         // play D
-        tone(8, notes[1]);
+        tone(8, noteD);
     }
     else if(keyVal >= 505 && keyVal <= 515){
         // play E
-        tone(8, notes[2]);
+        tone(8, noteE);
     }
     else if(keyVal >= 5 && keyVal <= 50){
         // play F
-        tone(8, notes[3]);
+        tone(8, noteF);
     }
     else {
         noTone(8);
